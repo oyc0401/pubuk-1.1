19b3af8b801a82a37cb3db2078fe88f1c97b8057
oyc0401 <oyc0401@gmail.com>
2021-08-28 13:21:24 +0900

    t

--- app/src/main/java/com/oyc0401/pubuk/MainActivity.java
+++ app/src/main/java/com/oyc0401/pubuk/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
