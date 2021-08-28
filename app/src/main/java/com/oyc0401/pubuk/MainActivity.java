c434235460ad415ae6701a39157a342e1a11afc4
oyc0401 <oyc0401@gmail.com>
2021-08-28 17:05:23 +0900

    꼬부기

--- app/src/main/java/com/oyc0401/pubuk/MainActivity.java
+++ app/src/main/java/com/oyc0401/pubuk/MainActivity.java
@@ -143,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
 //라이츄
 
 //피카츄
+        //꼬부기
 
         TextView tv_timetable321=findViewById(R.id.tv_timetable321);
         tv_timetable321.setOnClickListener(new View.OnClickListener() {
